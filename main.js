b5364f2e682447d33174d3ee8bda8e31b53884d8
LuxTSP <clubitusf@gmail.com>
2024-11-06 19:52:55 +0100

    Added some stuff
    - i think cashflow is working, not sure
    - InternSpd needs fixing
    
    code is shit

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
+//Refresh + Time function ------------------------------------------------------
 function cacheVal(){
-    document.getElementById('money').innerHTML = money;
-    document.getElementById('cashflow').innerHTML = cashflow;
+    money = parseInt(money);document.getElementById('money').innerHTML = money;
+    cashflow = (money - prmoney);prmoney = money;document.getElementById('cashflow').innerHTML = cashflow;
     document.getElementById('wareAmt').innerHTML = wareAmt;
     document.getElementById('printerLvl').innerHTML = prntLvl;
     document.getElementById('printerAmt').innerHTML =  prntAmt;
     document.getElementById('printerIntern').innerHTML = prntIntern ;
-    document.getElementById('printerInternSpd').innerHTML = prntInternSpd ;
-    document.getElementById('printerLvlPrice').innerHTML = prntLvlPrice ;
-    document.getElementById('printerAmtPrice').innerHTML = prntAmtPrice ;
-    document.getElementById('printerInternPrice').innerHTML = prntInternPrice ;
-    document.getElementById('printerInternSpdPrice').innerHTML = prntInternSpdPrice ;
+    document.getElementById('printerInternSpd').innerHTML = prntInternSpd * -1 ;
+    prntLvlPrice= parseInt(prntLvlPrice);document.getElementById('printerLvlPrice').innerHTML = prntLvlPrice ;
+    prntAmtPrice= parseInt(prntAmtPrice);document.getElementById('printerAmtPrice').innerHTML = prntAmtPrice ;
+    prntInternPrice= parseInt(prntInternPrice);document.getElementById('printerInternPrice').innerHTML = prntInternPrice ;
+    prntInternSpdPrice= parseInt(prntInternSpdPrice);document.getElementById('printerInternSpdPrice').innerHTML = prntInternSpdPrice ;
+    
 }
-let money = 1 ;
-let cashflow = 0;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//money + stats Values ---------------------------------------------------------
+let money = 1 ; let cashflow = 0; let prmoney = 1;
+//warehouse Values -------------------------------------------------------------
 let wareAmt = 0;
-let prntLvl = 0;
-let prntAmt = 0;
-let prntIntern = 0;
-let prntInternSpd = 0;
-let prntLvlPrice = 0;
-let prntAmtPrice = 0;
-let prntInternPrice = 25;
-let prntInternSpdPrice = 50;
-cacheVal();
-
-function printMoney() {
-	money += prntAmt*prntLvl;
-	cacheVal();
-}
+//Printer Values ---------------------------------------------------------------
+let prntLvl = 0;let prntAmt = 0;let prntIntern = 0;let prntInternSpd = 2000;
+let prntLvlPrice = 0;let prntAmtPrice = 0;let prntInternPrice = 25;let prntInternSpdPrice = 50;
+//------------------------------------------------------------------------------
+//Printer ----------------------------------------------------------------------
+function printMoney() { money += prntAmt*prntLvl; cacheVal();}
 function upgPrntAmt() {
 	if (money >= prntAmtPrice){ 
 		money -= prntAmtPrice;
@@ -38,6 +36,7 @@ function upgPrntAmt() {
             prntAmtPrice *= 1.75;
         }  
 		prntAmt+=1;
+        wareAmt+=1;
 		cacheVal();
 	}
 	
@@ -50,9 +49,42 @@ function upgPrntLvl() {
         }
         else{
             prntLvlPrice *= 1.3;
-        }  
+        }
+        
 		prntLvl+=1;
 		cacheVal();
 	}
 	
 }
+function upgPrntInt() {
+	if (money >= prntInternPrice){ 
+		money -= prntInternPrice;
+        prntInternPrice *= 1.7;  
+		prntIntern+=1;
+		cacheVal();
+	}
+}
+function upgPrntIntSpd() {
+	if (money >= prntInternSpdPrice){ 
+		
+        if (prntInternSpd > 200){
+            money -= prntInternSpdPrice;
+            prntInternSpdPrice *= 1.7;  
+		    prntInternSpd-=200;
+        } 
+        else{
+            prntInternSpd = 100;
+            printInternSpdPrice = 9999999999;
+        }
+        cacheVal();
+	}
+}
+setInterval(function() {
+    //printer
+   setInterval(function(){ money+= (prntIntern*(prntAmt*prntLvl)); }, prntInternSpd);
+    
+    cacheVal();
+}, 100);
+
+
+
